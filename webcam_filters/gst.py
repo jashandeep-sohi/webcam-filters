6e635e84a960fa481d7fcd5a77ea064c0ca337f7
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2021-10-11 19:57:01 -0700

    refactor: break-up the pipeline building/running logic a bit

--- webcam_filters/gst.py
+++ webcam_filters/gst.py
@@ -2,6 +2,7 @@ import typing as t
 import os
 import sys
 import enum
+import dataclasses
 
 import gi
 
@@ -48,6 +49,9 @@ class HardwareAccelAPI(enum.Enum):
     off = enum.auto()
     vaapi = enum.auto()
 
+    def __str__(self):
+      return self.name
+
 
 def init():
     # hack to let Gst.ElementFactory.make("...") to find custom plugins
@@ -70,252 +74,290 @@ def init():
         raise RuntimeError("failed to initialize gstreamer")
 
 
-def add_filters(
-    input_dev: str,
-    output_dev: str,
-    input_width: int,
-    input_height: int,
-    input_framerate: Fraction,
-    input_media_type: t.Optional[str],
-    background_blur: t.Optional[int],
-    selfie_segmentation_model: SelfieSegmentationModel,
-    selfie_segmentation_threshold: int,
-    hw_accel_api: HardwareAccelAPI,
-    verbose: bool,
-) -> None:
-    """
-    Run filters pipeline.
-    """
-    init()
+@dataclasses.dataclass()
+class Pipeline:
+    input_dev: str
+    output_dev: str
+    input_width: int
+    input_height: int
+    input_framerate: Fraction
+    input_media_type: t.Optional[str]
+    background_blur: t.Optional[int]
+    selfie_segmentation_model: SelfieSegmentationModel
+    selfie_segmentation_threshold: int
+    hw_accel_api: HardwareAccelAPI
+    verbose: bool
 
-    caps = query_device_caps(input_dev)
+    def run(self):
+        init()
 
-    if caps is None:
-        click.echo(f"unable to determine capabilities for device {input_dev!r}")
-        raise click.Abort()
+        self.enable_hwdec_plugins()
 
-    structs = []
-    for s in caps:
-        if input_media_type is not None and input_media_type != s.get_name():
-            continue
+        pipeline = self.build_pipeline()
 
-        s.fixate_field_nearest_int("width", input_width)
-        s.fixate_field_nearest_int("height", input_height)
-        s.fixate_field_nearest_fraction(
-            "framerate",
-            input_framerate.numerator,
-            input_framerate.denominator
+        loop = GObject.MainLoop()
+
+        if self.verbose:
+            pipeline.add_property_deep_notify_watch(None, True)
+
+        bus = pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_bus_message, loop, pipeline)
+
+        pipeline.set_state(Gst.State.PLAYING)
+        try:
+            loop.run()
+        except KeyboardInterrupt:
+            loop.quit()
+
+        pipeline.set_state(Gst.State.NULL)
+
+    def select_input(self) -> Gst.Caps:
+        """
+        Return Caps for input device that's closest to the desired values.
+        """
+        dev = self.input_dev
+        media_type = self.input_media_type
+        width = self.input_width
+        height = self.input_height
+        framerate = self.input_framerate
+
+        caps = query_device_caps(dev)
+
+        if caps is None:
+            click.echo(f"unable to determine capabilities for device {dev!r}")
+            raise click.Abort()
+
+        structs = []
+        for s in caps:
+            if media_type is not None and media_type != s.get_name():
+                continue
+
+            s.fixate_field_nearest_int("width", width)
+            s.fixate_field_nearest_int("height", height)
+            s.fixate_field_nearest_fraction(
+                "framerate",
+                framerate.numerator,
+                framerate.denominator
+            )
+            structs.append(s)
+
+        if not structs:
+            click.echo("unable to find a suitable input format")
+            raise click.Abort()
+
+        def struct_sort_key(x):
+            _, xwidth = x.get_int("width")
+            _, xheight = x.get_int("height")
+            _, fr_numerator, fr_denomantor = x.get_fraction("framerate")
+            return (
+                abs(width - xwidth),
+                abs(height - xheight),
+                abs(framerate - Fraction(fr_numerator, fr_denomantor or 1))
+            )
+
+        structs = sorted(structs, key=struct_sort_key)
+
+        s = structs[0]
+        new_caps = Gst.Caps.new_empty()
+        new_caps.append_structure(s)
+
+        return new_caps
+
+    def build_pipeline(self) -> Gst.Pipeline:
+        input_caps = self.select_input()
+        click.echo(f"Selected input: {input_caps}")
+
+        pipeline = Gst.Pipeline.new()
+
+        src = Gst.ElementFactory.make("v4l2src")
+        inputfilter = Gst.ElementFactory.make("capsfilter")
+        decodebin = Gst.ElementFactory.make("decodebin3")
+
+        if self.hw_accel_api == HardwareAccelAPI.vaapi:
+            # vaapipostproc doesn't seem to support going directly to RGB on some
+            # hardware. So prefer direct RGB conversion if possible, otherwise
+            # fallback to some variant of RGBA and then use videoconvert to
+            # to get to RGB.
+            c= ";".join(f"video/x-raw, format={f}" for f in reversed(RGB_FORMATS))
+            rgbconvert = Gst.parse_bin_from_description(
+                f"capsfilter caps=video/x-raw(memory:VASurface) ! "
+                f"vaapipostproc ! {c} ! videoconvert",
+                True
+            )
+        else:
+            rgbconvert = Gst.ElementFactory.make("videoconvert")
+
+        rgbfilter = Gst.ElementFactory.make("capsfilter")
+        tee = Gst.ElementFactory.make("tee")
+
+        if self.hw_accel_api == HardwareAccelAPI.vaapi:
+            c = ";".join(f"video/x-raw, format={f}" for f in RGB_FORMATS)
+            sinkconvert = Gst.parse_bin_from_description(
+                f"videoconvert ! {c} ! vaapipostproc",
+                True
+            )
+        else:
+            sinkconvert = Gst.ElementFactory.make("videoconvert")
+
+        sinkfilter = Gst.ElementFactory.make("capsfilter")
+        sink = Gst.ElementFactory.make("v4l2sink")
+
+        src.set_property("device", self.input_dev)
+        inputfilter.set_property("caps", input_caps)
+        rgbfilter.set_property(
+            "caps",
+            Gst.Caps.from_string("video/x-raw, format=RGB")
         )
-        structs.append(s)
+        sinkfilter.set_property(
+            "caps",
+            Gst.Caps.from_string(f"video/x-raw, format=YUY2")
+        )
+        sink.set_property("device", self.output_dev)
+        sink.set_property("throttle-time", 10)
+        sink.set_property("qos", True)
 
-    if not structs:
-        click.echo("unable to find a suitable input format")
-        raise click.Abort()
-
-    def struct_sort_key(x):
-        _, width = x.get_int("width")
-        _, height = x.get_int("height")
-        _, fr_numerator, fr_denomantor = x.get_fraction("framerate")
-        return (
-            abs(input_width - width),
-            abs(input_height - height),
-            abs(input_framerate - Fraction(fr_numerator, fr_denomantor or 1))
+        pipeline.add(
+            src,
+            inputfilter,
+            decodebin,
+            rgbconvert,
+            rgbfilter,
+            tee,
+            sinkconvert,
+            sinkfilter,
+            sink,
         )
 
-    structs = sorted(structs, key=struct_sort_key)
-
-    s = structs[0]
-    new_caps = Gst.Caps.new_empty()
-    new_caps.append_structure(s)
-
-    click.echo(
-        f"Selected input: media-type={s.get_name()}, width={s.get_value('width')} "
-        f"height={s.get_value('height')} framerate={s.get_value('framerate')}"
-    )
-
-    enable_hwdec_plugins(hw_accel_api)
-
-    pipeline = Gst.Pipeline.new()
-
-    src = Gst.ElementFactory.make("v4l2src")
-    inputfilter = Gst.ElementFactory.make("capsfilter")
-    decodebin = Gst.ElementFactory.make("decodebin3")
-
-    if hw_accel_api == HardwareAccelAPI.vaapi:
-        # vaapipostproc doesn't seem to support going directly to RGB on some
-        # hardware. So prefer direct RGB conversion if possible, otherwise
-        # fallback to some variant of RGBA and then use videoconvert to
-        # to get to RGB.
-        c= ";".join(f"video/x-raw, format={f}" for f in reversed(RGB_FORMATS))
-        rgbconvert = Gst.parse_bin_from_description(
-            f"capsfilter caps=video/x-raw(memory:VASurface) ! "
-            f"vaapipostproc ! {c} ! videoconvert",
-            True
+        src.link(inputfilter)
+        inputfilter.link(decodebin)
+        decodebin.connect(
+            "pad-added",
+            lambda dbin, pad: pad.link(rgbconvert.get_static_pad("sink"))
         )
-    else:
-        rgbconvert = Gst.ElementFactory.make("videoconvert")
+        rgbconvert.link(rgbfilter)
+        rgbfilter.link(tee)
 
-    rgbfilter = Gst.ElementFactory.make("capsfilter")
-    tee = Gst.ElementFactory.make("tee")
+        out = tee
 
-    if hw_accel_api == HardwareAccelAPI.vaapi:
-        c = ";".join(f"video/x-raw, format={f}" for f in RGB_FORMATS)
-        sinkconvert = Gst.parse_bin_from_description(
-            f"videoconvert ! {c} ! vaapipostproc",
-            True
-        )
-    else:
-        sinkconvert = Gst.ElementFactory.make("videoconvert")
+        need_selfie = self.background_blur or False
 
-    sinkfilter = Gst.ElementFactory.make("capsfilter")
-    sink = Gst.ElementFactory.make("v4l2sink")
+        if need_selfie:
+            selfie_queue = Gst.ElementFactory.make("queue", "selfie_queue")
+            selfie = Gst.ElementFactory.make("selfie_seg")
+            selfie.set_property("model", self.selfie_segmentation_model)
+            selfie.set_property("threshold", self.selfie_segmentation_threshold)
+            pipeline.add(selfie_queue, selfie)
+            tee.link(selfie_queue)
+            selfie_queue.link(selfie)
 
-    src.set_property("device", input_dev)
-    inputfilter.set_property("caps", new_caps)
-    rgbfilter.set_property(
-        "caps",
-        Gst.Caps.from_string("video/x-raw, format=RGB")
-    )
-    sinkfilter.set_property(
-        "caps",
-        Gst.Caps.from_string(f"video/x-raw, format=YUY2")
-    )
-    sink.set_property("device", output_dev)
-    sink.set_property("throttle-time", 10)
-    sink.set_property("qos", True)
+        if self.background_blur:
+            blured_queue = Gst.ElementFactory.make("queue", "blured_queue")
+            blured = Gst.ElementFactory.make("cv2_boxfilter")
+            blured.set_property("ksize", self.background_blur)
+            pipeline.add(blured_queue, blured)
+            tee.link(blured_queue)
+            blured_queue.link(blured)
 
-    pipeline.add(
-        src,
-        inputfilter,
-        decodebin,
-        rgbconvert,
-        rgbfilter,
-        tee,
-        sinkconvert,
-        sinkfilter,
-        sink,
-    )
+            where = Gst.ElementFactory.make("numpy_where")
+            pipeline.add(where)
+            selfie.get_static_pad("src").link(where.get_static_pad("condition"))
+            tee.request_pad(tee.get_pad_template("src_%u"), None, None).link(
+                where.get_static_pad("x")
+            )
+            blured.get_static_pad("src").link(where.get_static_pad("y"))
 
-    src.link(inputfilter)
-    inputfilter.link(decodebin)
-    decodebin.connect(
-        "pad-added",
-        lambda dbin, pad: pad.link(rgbconvert.get_static_pad("sink"))
-    )
-    rgbconvert.link(rgbfilter)
-    rgbfilter.link(tee)
+            out = where
 
-    out = tee
+        out.link(sinkconvert)
+        sinkconvert.link(sinkfilter)
+        sinkfilter.link(sink)
 
-    need_selfie = background_blur or False
-
-    if need_selfie:
-        selfie_queue = Gst.ElementFactory.make("queue", "selfie_queue")
-        selfie = Gst.ElementFactory.make("selfie_seg")
-        selfie.set_property("model", selfie_segmentation_model)
-        selfie.set_property("threshold", selfie_segmentation_threshold)
-        pipeline.add(selfie_queue, selfie)
-        tee.link(selfie_queue)
-        selfie_queue.link(selfie)
-
-    if background_blur:
-        blured_queue = Gst.ElementFactory.make("queue", "blured_queue")
-        blured = Gst.ElementFactory.make("cv2_boxfilter")
-        blured.set_property("ksize", background_blur)
-        pipeline.add(blured_queue, blured)
-        tee.link(blured_queue)
-        blured_queue.link(blured)
-
-        where = Gst.ElementFactory.make("numpy_where")
-        pipeline.add(where)
-        selfie.get_static_pad("src").link(where.get_static_pad("condition"))
-        tee.request_pad(tee.get_pad_template("src_%u"), None, None).link(
-            where.get_static_pad("x")
-        )
-        blured.get_static_pad("src").link(where.get_static_pad("y"))
-
-        out = where
-
-    out.link(sinkconvert)
-    sinkconvert.link(sinkfilter)
-    sinkfilter.link(sink)
+        return pipeline
 
 
-    loop = GObject.MainLoop()
+    def enable_hwdec_plugins(self) -> None:
+        if self.hw_accel_api == HardwareAccelAPI.off:
+            return
 
-    if verbose:
-        pipeline.add_property_deep_notify_watch(None, True)
+        reg = Gst.Registry.get()
 
-    bus = pipeline.get_bus()
-    bus.add_signal_watch()
-    bus.connect("message", on_bus_message, loop, verbose, pipeline)
+        if self.hw_accel_api == HardwareAccelAPI.vaapi:
+            plugins = [
+                "vaapijpegdec",
+                "vaapimpeg2dec",
+                "vaapih264dec",
+                "vaapih265dec",
+                "vaapivc1dec",
+            ]
 
-    pipeline.set_state(Gst.State.PLAYING)
-    try:
-        loop.run()
-    except KeyboardInterrupt:
-        loop.quit()
+            for p in plugins:
+                factory = Gst.ElementFactory.find(p)
 
-    pipeline.set_state(Gst.State.NULL)
+                if factory is None:
+                    continue
 
+                factory.set_rank(Gst.Rank.PRIMARY + 1)
 
-def on_bus_message(
-    bus: Gst.Bus,
-    message: Gst.Message,
-    loop: GObject.MainLoop,
-    verbose: bool,
-    pipeline: Gst.Pipeline,
-) -> bool:
-    mtype = message.type
-    src = message.src
-    src_path = src.get_path_string()
+                reg.add_feature(factory)
 
-    if verbose:
-        click.echo(f"\nsrc: {src_path}")
-        click.echo(f"type: {mtype}")
+    def on_bus_message(
+        self,
+        bus: Gst.Bus,
+        message: Gst.Message,
+        loop: GObject.MainLoop,
+        pipeline: Gst.Pipeline,
+    ) -> bool:
+        mtype = message.type
+        src = message.src
+        src_path = src.get_path_string()
 
-    if mtype == Gst.MessageType.EOS:
-        loop.quit()
+        if self.verbose:
+            click.echo(f"\nsrc: {src_path}")
+            click.echo(f"type: {mtype}")
 
-    elif mtype == Gst.MessageType.ERROR:
-        gerror, debug = message.parse_error()
-        click.echo(f"Error from {src_path}: {gerror.message}")
-        if verbose:
-            click.echo(f"Debug: {debug}")
+        if mtype == Gst.MessageType.EOS:
+            loop.quit()
 
-        loop.quit()
+        elif mtype == Gst.MessageType.ERROR:
+            gerror, debug = message.parse_error()
+            click.echo(f"Error from {src_path}: {gerror.message}")
+            if self.verbose:
+                click.echo(f"Debug: {debug}")
 
-    elif mtype == Gst.MessageType.WARNING:
-        gerror, debug = message.parse_warning()
-        click.echo(f"Warning from {src_path}: {gerror.message}")
-        if verbose:
-            click.echo(f"Debug: {debug}")
+            loop.quit()
 
-    elif mtype == Gst.MessageType.INFO:
-        gerror, debug = message.parse_info()
-        click.echo(f"Info from {src_path}: {gerror.message}")
-        if verbose:
-            click.echo(f"Debug: {debug}")
+        elif mtype == Gst.MessageType.WARNING:
+            gerror, debug = message.parse_warning()
+            click.echo(f"Warning from {src_path}: {gerror.message}")
+            if self.verbose:
+                click.echo(f"Debug: {debug}")
 
-    elif mtype == Gst.MessageType.PROPERTY_NOTIFY and verbose:
-        obj, name, value = message.parse_property_notify()
+        elif mtype == Gst.MessageType.INFO:
+            gerror, debug = message.parse_info()
+            click.echo(f"Info from {src_path}: {gerror.message}")
+            if self.verbose:
+                click.echo(f"Debug: {debug}")
 
-        click.echo(f"{name}: {value}")
+        elif mtype == Gst.MessageType.PROPERTY_NOTIFY and self.verbose:
+            obj, name, value = message.parse_property_notify()
 
-    elif mtype == Gst.MessageType.STATE_CHANGED and src == pipeline:
-        old, new, pending = message.parse_state_changed()
+            click.echo(f"{name}: {value}")
 
-        click.echo("Pipeline: ", nl=False)
-        if new == Gst.State.PAUSED:
-            click.echo("PAUSED")
-        elif new == Gst.State.READY:
-            click.echo("READY")
-        elif new == Gst.State.PLAYING:
-            click.echo("RUNNING")
-        elif new == Gst.State.NULL:
-            click.echo("NULL")
+        elif mtype == Gst.MessageType.STATE_CHANGED and src == pipeline:
+            old, new, pending = message.parse_state_changed()
 
-    return True
+            click.echo("Pipeline: ", nl=False)
+            if new == Gst.State.PAUSED:
+                click.echo("PAUSED")
+            elif new == Gst.State.READY:
+                click.echo("READY")
+            elif new == Gst.State.PLAYING:
+                click.echo("RUNNING")
+            elif new == Gst.State.NULL:
+                click.echo("NULL")
+
+        return True
 
 
 def query_device_caps(dev: str) -> t.Optional[Gst.Caps]:
@@ -376,29 +418,3 @@ def print_device_caps(
     console.print(table)
 
     ctx.exit(0)
-
-
-def enable_hwdec_plugins(api: HardwareAccelAPI) -> None:
-    if api == HardwareAccelAPI.off:
-        return
-
-    reg = Gst.Registry.get()
-
-    if api == HardwareAccelAPI.vaapi:
-        plugins = [
-            "vaapijpegdec",
-            "vaapimpeg2dec",
-            "vaapih264dec",
-            "vaapih265dec",
-            "vaapivc1dec",
-        ]
-
-        for p in plugins:
-            factory = Gst.ElementFactory.find(p)
-
-            if factory is None:
-                continue
-
-            factory.set_rank(Gst.Rank.PRIMARY + 1)
-
-            reg.add_feature(factory)
